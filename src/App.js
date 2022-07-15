6a4d5f538d7cc680fd18edbb5f83fb758da551ca
skarfie123 <rahulpai@rocketmail.com>
2022-07-15 16:45:30 +0100

    compare html and react form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./logo.svg";
+import ReactForm from "./ReactForm";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Tell me a secret</h1>
+        <div class="row">
+          <div class="column">
+            <h2>Via a html form</h2>
+            <form>
+              <label for="secret1">Secret 1:</label>
+              <br />
+              <input type="password" id="secret1" name="secret1" />
+              <input type="submit" value="Submit" />
+            </form>
+          </div>
+          <div class="column">
+            <h2>Via a react form</h2>
+            <ReactForm />
+          </div>
+        </div>
       </header>
     </div>
   );
---
+++ src/ReactForm.js
@@ -0,0 +1,42 @@
+﻿import React from "react";
+class ReactForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { secret2: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    this.setState({
+      [target.name]: target.value,
+    });
+  }
+
+  handleSubmit(event) {
+    alert("secret2: " + this.state.secret2);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Secret 2:
+          <br />
+          <input
+            name="secret2"
+            type="password"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+export default ReactForm;
